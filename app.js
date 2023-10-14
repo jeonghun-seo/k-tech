076ef694ee8fa21c5e18130b51b9099f938826dd
JeongHun-Seo <jeonghun.seo@outlook.kr>
2023-10-14 22:38:51 +0900

    add comment

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 //app.js
-const express = require('express') //express 모듈을 가져온다.
+const express = require('express') //express 모듈을 가져온다
 const app = express() //가져온 express 모듈의 function을 이용해서 새로운 express 앱을 만든다
 const port = 3000 
 
 const fs = require('fs');
 
 app.get('/', (req, res) => {
-    const filePath = 'log/detected_log.txt';
+    const filePath = 'log/detected_log.txt'; //log파일 읽기
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             console.error(err);
-            return res.status(500).send('Error reading file');
+            return res.status(500).send('Error reading file'); // log파일을 읽지 못할 경우, 서버 스크립트 오류에 사용되는 500 에러 반환
         }
-            res.send(data);
+            res.send(data); //log 출력
         });
     });
 
